6d18745e621bf607a6a4d4d4af96c8e630d4ece6
Stephan Mazur <stephan@9tq.fr>
2023-02-20 20:53:16 +0100

    wip: Mutations generator (missing attributes file)

--- lib/generators/ntq_tools/graphql_scaffold_generator.rb
+++ lib/generators/ntq_tools/graphql_scaffold_generator.rb
@@ -2,8 +2,8 @@ module NtqTools
   module Generators
     SKIPPED_ATTRIBUTE_NAMES = %w[created_at updated_at]
     class GraphqlScaffoldGenerator < Rails::Generators::NamedBase
-      source_root File.expand_path("../../templates", __FILE__)
-    
+      source_root File.expand_path('../templates', __dir__)
+
       def create_type_file
         singular_name = plural_name.singularize
         return unless defined?(class_name.constantize) && class_name.constantize.respond_to?(:columns_hash)
@@ -16,11 +16,11 @@ module NtqTools
           when :string, :text
             definition << "field :#{k}, String"
           when :integer
-            if k.include?('_id') || k == 'id'
-              definition << "field :#{k}, ID"
-            else
-              definition << "field :#{k}, Integer"
-            end
+            definition << if k.include?('_id') || k == 'id'
+                            "field :#{k}, ID"
+                          else
+                            "field :#{k}, Integer"
+                          end
           when :boolean
             definition << "field :#{k}, Boolean"
           when :decimal
@@ -32,20 +32,112 @@ module NtqTools
           end
         end
 
-        create_file "app/graphql/types/#{plural_name}/#{singular_name}_type.rb", <<-FILE
-module Types
-  module #{plural_name.capitalize}
-    class #{singular_name.capitalize}Type < BaseObject
-      include Resolvers::TimestampsFields
+        p definition
 
-      #{definition.join('
-      ')}
-    end
-  end
-end
-    FILE
+        # Type file
+        create_file "app/graphql/types/#{plural_name}/#{singular_name}_type.rb", <<~FILE
+          module Types
+            module #{plural_name.capitalize}
+              class #{singular_name.capitalize}Type < BaseObject
+                include Resolvers::TimestampsFields
+
+                #{definition.join('
+                ')}
+              end
+            end
+          end
+        FILE
+
+        # Create mutation
+        create_file "app/graphql/mutations/#{plural_name}/create_#{singular_name}.rb", <<~FILE
+          module Mutations
+            class #{plural_name.capitalize}::Create#{singular_name.capitalize} < BaseMutation
+              field :flash_messages, [Types::JsonType], null: false
+
+              argument :attributes, InputObject::#{plural_name.capitalize}Attributes, required: true
+
+              def authorized?(attributes:)
+                context[:current_user].can?(:manage, #{singular_name})
+              end
+
+              def resolve(attributes:)
+                object = #{singular_name.capitalize}.new
+                object.attributes = attributes
+                object.save!
+                {
+                  flash_messages: [
+                    #{singular_name} = object,
+                    {
+                      type: 'success',
+                      message: I18n.t(:'flashes.#{plural_name}.create_#{singular_name}.success')
+                    }
+                  ]
+                }
+              end
+            end
+          end
+        FILE
+
+        # Delete mutation
+        create_file "app/graphql/mutations/#{plural_name}/delete_#{singular_name}.rb", <<~FILE
+          module Mutations
+            class #{plural_name.capitalize}::Delete#{singular_name.capitalize} < BaseMutation
+              field :flash_messages, [Types::JsonType], null: false
+
+              argument :id, ID, required: true
+
+              def authorized?(id:)
+                #{singular_name} = #{singular_name.capitalize}.find id
+                context[:current_user].can?(:manage, #{singular_name})
+              end
+
+              def resolve(id:)
+                #{singular_name} = #{singular_name.capitalize}.find id
+                #{singular_name}.destroy!
+                {
+                  flash_messages: [
+                    {
+                      type: 'success',
+                      message: I18n.t(:'flashes.#{plural_name}.delete_#{singular_name}.success')
+                    }
+                  ]
+                }
+              end
+            end
+          end
+        FILE
+
+        # Update mutation
+        create_file "app/graphql/mutations/#{plural_name}/update_#{singular_name}.rb", <<~FILE
+          module Mutations
+            class #{plural_name.capitalize}::Update#{singular_name.capitalize} < BaseMutation
+              field :flash_messages, [Types::JsonType], null: false
+
+              argument :id, ID, required: true
+              argument :attributes, InputObject::#{plural_name.capitalize}Attributes, required: true
+
+              def authorized?(attributes:, id:)
+                #{singular_name} = #{singular_name.capitalize}.find id
+                context[:current_user].can?(:manage, #{singular_name})
+              end
+
+              def resolve(attributes:, id:)
+                object = #{singular_name.capitalize}.find id
+                object.attributes = attributes
+                object.save!
+                {
+                  flash_messages: [
+                    {
+                      type: 'success',
+                      message: I18n.t(:'flashes.#{plural_name}.update_#{singular_name}.success')
+                    }
+                  ]
+                }
+              end
+            end
+          end
+        FILE
       end
-
     end
   end
 end
